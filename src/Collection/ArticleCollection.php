b910e0fc1af2e94a75b7a7aeca7dd50bd8ea6f80
alebediev <44255630+alebediev@users.noreply.github.com>
2019-10-29 17:14:41 +0200

    Article page

--- src/Collection/ArticleCollection.php
+++ src/Collection/ArticleCollection.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the "PHP Static Analyzer" project.
+ *
+ * (c) Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Collection;
 
 use App\Model\Article;
@@ -8,24 +19,30 @@ final class ArticleCollection implements \IteratorAggregate
 {
     /** @var Article[] */
     private $articles;
+
     public function __construct(Article ...$articles)
     {
         $this->articles = $articles;
     }
+
     public function shift(): Article
     {
         $article = \array_shift($this->articles);
+
         if (null === $article) {
             throw new \RuntimeException('Articles collection is empty');
         }
+
         return $article;
     }
+
     public function slice(int $number): iterable
     {
-        for ($i = 0; $i < $number; $i++) {
+        for ($i = 0; $i < $number; ++$i) {
             yield $this->shift();
         }
     }
+
     public function getIterator(): iterable
     {
         return new \ArrayIterator($this->articles);
--- src/Service/Article/FakeArticlePresentationService.php
+++ src/Service/Article/FakeArticlePresentationService.php
@@ -1,12 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the "PHP Static Analyzer" project.
+ *
+ * (c) Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Service\Article;
 
 use App\Collection\ArticleCollection;
 use App\Model\Article;
 use Faker\Factory;
 
-final class FakeArticlePresentationService implements ArticlePresentationInterface
+final class FakeArticlePresentationService implements ArticlePresentationInterface, ArticlePageInterface
 {
     private const CATEGORIES = [
         'World',
@@ -14,11 +25,13 @@ final class FakeArticlePresentationService implements ArticlePresentationInterfa
         'IT',
         'Science',
     ];
+
     public function getLatest(): ArticleCollection
     {
         $faker = Factory::create();
         $articles = [];
-        for ($i = 0; $i < 10; $i++) {
+
+        for ($i = 0; $i < 10; ++$i) {
             $article = new Article(
                 $faker->numberBetween(1, 10),
                 $faker->randomElement(self::CATEGORIES),
@@ -31,6 +44,29 @@ final class FakeArticlePresentationService implements ArticlePresentationInterfa
             $article->setImage($faker->imageUrl());
             $articles[] = $article;
         }
+
         return new ArticleCollection(...$articles);
+    }
+
+    /**
+     * Generate fake article.
+     *
+     * @param int $id
+     *
+     * @return Article
+     */
+    public function getArticle(int $id): Article
+    {
+        $faker = Factory::create();
+        $article = new Article($id,
+            $faker->randomElement(self::CATEGORIES),
+            $faker->words($faker->numberBetween(3, 5), true),
+            \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-7 days'))
+        );
+        $article->setDescription($faker->words($faker->numberBetween(4, 8), true));
+        $article->setBody($faker->realText(3000));
+        $article->setImage($faker->imageUrl());
+
+        return $article;
     }
 }
