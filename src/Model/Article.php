287c09e5d71cf2b6838357925c3658b187ce1740
alebediev <44255630+alebediev@users.noreply.github.com>
2019-10-30 16:33:52 +0200

    Change headers

--- src/Model/Article.php
+++ src/Model/Article.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "PHP Static Analyzer" project.
+ * This file is part of the "News portal" project.
  *
- * (c) Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * (c) Andrii Lebediev <wasaby.stnc@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
